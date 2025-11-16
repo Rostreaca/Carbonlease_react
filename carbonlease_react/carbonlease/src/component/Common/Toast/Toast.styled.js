5bcab0a5180bc15ff967bc76f0ac89994f3abf42
suelina001 <elinaen001@gmail.com>
2025-11-16 21:32:04 +0900

    스타일 수정

--- carbonlease_react/carbonlease/src/component/Common/Toast/Toast.styled.js
+++ carbonlease_react/carbonlease/src/component/Common/Toast/Toast.styled.js
@@ -37,8 +37,7 @@ const variantColors = {
 };
 
 export const StyledToast = styled(Toast)`
-    min-width: 300px;
-    max-width: 500px;
+    width:auto;
     border-radius: 12px;
     box-shadow: 0 8px 24px rgba(0, 0, 0, 0.15);
     border: 2px solid ${props => variantColors[props.$variant]?.border || variantColors.success.border};
