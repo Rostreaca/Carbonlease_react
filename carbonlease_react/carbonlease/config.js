e52dc62d8cc7cca39897e55f83d463df17eec926
suelina <suelina001@gmail.com>
2025-12-30 15:38:15 +0900

    잠시 보류

--- carbonlease_react/carbonlease/config.js
+++ carbonlease_react/carbonlease/config.js
@@ -1,19 +1,12 @@
-// window.ENV = {
-//     API_URL: "https://sh-pk.store",
-//     WS_URL: "https://sh-pk.store/ws-stomp",
-//     KAKAO_REDIRECT_URI: "https://sh-pk.store/kakao/callback",
-//     KAKAO_MAP_API_KEY: "7317ce02b514818da5c2a34210dc75ac", // JavaScript 키
-//     KAKAO_CLIENT_ID: "6caaa3a7fb9c9fd967792899e6949126" // REST API 키
-// }
-
+const isLocal = window.location.hostname === "localhost";
 
 window.ENV = {
-    API_URL: "http://localhost:8080",
-    WS_URL: "http://localhost:8080/ws-stomp",
-    KAKAO_REDIRECT_URI: "http://localhost:8080/kakao/callback",
-    KAKAO_MAP_API_KEY: "7317ce02b514818da5c2a34210dc75ac", // JavaScript 키
-    KAKAO_CLIENT_ID: "6caaa3a7fb9c9fd967792899e6949126" // REST API 키
-}
+    API_URL: isLocal ? "http://localhost:8080" : "https://sh-pk.store",
+    WS_URL: isLocal ? "http://localhost:8080/ws-stomp" : "https://sh-pk.store/ws-stomp",
+    KAKAO_REDIRECT_URI: isLocal ? "http://localhost:8080/kakao/callback" : "https://sh-pk.store/kakao/callback",
+    KAKAO_MAP_API_KEY: "7317ce02b514818da5c2a34210dc75ac",
+    KAKAO_CLIENT_ID: "6caaa3a7fb9c9fd967792899e6949126"
+};
 
 
 
